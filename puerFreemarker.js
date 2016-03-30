f9d4ea0edf01d458a6172866a8d191ab617bb979
Hendrik Wallbaum <mail@hendrikwallbaum.de>
2016-03-30 15:06:42 +0800

    Both files are combined into a string that represents the combined file.

--- puerFreemarker.js
+++ puerFreemarker.js
@@ -1,12 +1,23 @@
 var cli = require('commander');
 
+//Submodules
+var processor = require('./routePreProcessor');
+
 //Configure commandline usage.
 cli
     .usage('puerF [options]')
     .option('-f, --freemarker <file>', 'Mock file for Freemarker routes')
     .option('-m, --mock <file>', 'Your standard puer mock file')
+    .option('-c, --combined <file>', 'Where to save the combined file, defaults to "mock/allRoutes.js"')
     .option('-t, --templates <path>', 'Path to folder in which Freemarker templates are stored')
     .option('-p, --port', 'Specific port to use')
     .option('-w, --watch', 'Filetypes to watch, defaults to js|css|html|xhtml')
     .option('-x, --exclude', 'Exclude files from being watched for updates')
     .parse(process.argv);
+
+var ftlRoutesFile = cli.freemarker || 'mock/ftlRoutes';
+var routesFile = cli.mock || 'mock/routes';
+var combinedFile = cli.combined || 'mock/allRoutes.js';
+var templatesPath = cli.templates || 'templates';
+
+processor.process(routesFile, ftlRoutesFile, combinedFile, templatesPath);
---
+++ routePreProcessor.js
@@ -0,0 +1,76 @@
+/**
+    A module to process routes.js and tflRoutes.js files into a single file.
+
+    module.process(routesFile, ftlRoutesFile, combinedFile, templatesPath)
+    @param routesFile       The path the the routes file.
+    @param ftlRoutesFile    Path to the file containing tflRoutes.
+    @param combinedFile     The file in which to save the combined version.
+    @param templatesPath    Where templates can be found.
+
+*/
+module.exports = function routePreProcessor() {
+
+    /**
+     *   Process both the regular and the ftlRoutes file into a single one.
+     */
+    function processFiles(routesFile, ftlRoutesFile, combinedFile, templatesPath) {
+        routesFile = './' + routesFile.replace('.js', '');
+        ftlRoutesFile = './' + ftlRoutesFile.replace('.js', '');
+        var routes = require(routesFile);
+        var ftlRoutes = require(ftlRoutesFile);
+        for (key in ftlRoutes) {
+            routes[key] = convertFtl(ftlRoutes[key]);
+        }
+        var combined = createCombinedFile(routes, templatesPath);
+        console.log(combined);
+    }
+
+    /**
+     *   Converts a ftlRoutes config object so that it can be added into the combined routes file.
+     */
+    function convertFtl(config) {
+        return function() {
+            fm.render(config.template, config.data, function(err, data, out) {
+                res.writeHeader(200, {
+                    "Content-Type": "text/html"
+                });
+                res.write(data);
+                res.end();
+            });
+        }
+    }
+
+    /**
+     *   Uses all routes to write a file that can be used with puer.
+     */
+    function createCombinedFile(routes, templatesPath) {
+        var start = `var Freemarker = require('Freemarker');
+            var fm = new Freemarker({
+              viewRoot: path.join(__dirname, '${templatesPath}')
+            });
+            module.exports = {`;
+        var end = `}`;
+        var middle = createMiddleString(routes);
+        var combined = start + middle + end;
+        return combined;
+    }
+
+    /**
+     *   Parses routes into a string.
+     */
+    function createMiddleString(routes) {
+        var string = '';
+        for (key in routes) {
+            string += `\n"${key}": ${routes[key]},`;
+        }
+
+        //Remove the last ','.
+        string = string.replace(new RegExp(',' + '$'), '\n');
+        return string;
+    }
+
+    return {
+        process: processFiles
+    }
+
+}();
