bbc5d365a7fc39d44f801b3a0bd8580cc7715d90
Hendrik Wallbaum <mail@hendrikwallbaum.de>
2016-03-30 16:28:17 +0800

    More docu in main file

--- puerFreemarker.js
+++ puerFreemarker.js
@@ -15,9 +15,19 @@ cli
     .option('-x, --exclude', 'Exclude files from being watched for updates')
     .parse(process.argv);
 
+//Path to ftlRoutes file.
 var ftlRoutesFile = cli.freemarker || 'mock/ftlRoutes';
+
+//Path to routes file.
 var routesFile = cli.mock || 'mock/routes';
+
+//Path to combined file.
 var combinedFile = cli.combined || 'mock/allRoutes.js';
+
+//Root directory for templates.
 var templatesPath = cli.templates || 'templates';
 
+//Initially generate a combined version of the routes files.
 processor.process(routesFile, ftlRoutesFile, combinedFile, templatesPath);
+
+//Watch route files for changes and act upon them.
