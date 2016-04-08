cf52dbc7120c597646bd8ae83903f4195ea565f2
Hendrik Wallbaum <mail@hendrikwallbaum.de>
2016-04-08 13:07:44 +0800

    Testing with files and cleanups

--- test/testHelper.js
+++ test/testHelper.js
@@ -1,11 +1,33 @@
+var path = require('path');
+var fs = require('fs-extra');
+var helper = require('../bin/helper');
 module.exports = function(test) {
+
+    var tmpPath = path.join(__dirname, 'tmp');
 
     test('Testing helper functions', function(t) {
 
         //First clear the tmp folder.
-        fs.unlinkSync('./tmp');
-        
+        if(fs.existsSync(tmpPath)) fs.removeSync(tmpPath);
 
+        helper.guarantyFolder(tmpPath);
+        t.ok(fs.existsSync(tmpPath), 'Creates folders');
+
+        var pathToFile = path.join(tmpPath, 'tstFolder', 'file.txt');
+        var pathToFolder = path.join(tmpPath, 'tstFolder');
+        helper.guarantyFolder(pathToFile);
+        t.ok(fs.existsSync(pathToFolder), 'Creates folder when given file path');
+
+        //Cleanup after ourselfes.
+        fs.removeSync(tmpPath);
+
+        t.end();
+
+/*
+        loadModule: loadModuleWithoutCache,
+        guarantyFolder: makeSureFolderExists,
+        absolutePath: getAbsolutePath
+*/
     });
 
 }
