0de2d7898efa2e5dc12482138c3232fba4b31190
Hendrik Wallbaum <mail@hendrikwallbaum.de>
2016-04-08 13:23:39 +0800

    Testing helper now

---
+++ test/assets/testModuleChanged.js
@@ -0,0 +1,5 @@
+//This module is to test if helper gets changed modules without chace.
+module.exports = {
+    name: 'Tester',
+    changed: true
+}
--- test/testHelper.js
+++ test/testHelper.js
@@ -1,3 +1,12 @@
+/**
+    Testing bin/helper.js
+
+    Interface tested:
+    loadModule: loadModuleWithoutCache,
+    guarantyFolder: makeSureFolderExists,
+    absolutePath: getAbsolutePath
+*/
+
 var path = require('path');
 var fs = require('fs-extra');
 var helper = require('../bin/helper');
@@ -5,29 +14,46 @@ module.exports = function(test) {
 
     var tmpPath = path.join(__dirname, 'tmp');
 
-    //First clear the tmp folder.
-    if(fs.existsSync(tmpPath)) fs.removeSync(tmpPath);
-
     test('Testing helper', function(t) {
 
+        //First clear the tmp folder.
+        if(fs.existsSync(tmpPath)) fs.removeSync(tmpPath);
+
+        //Guaranty folder
         helper.guarantyFolder(tmpPath);
         t.ok(fs.existsSync(tmpPath), 'Creates folders');
-
         var pathToFile = path.join(tmpPath, 'tstFolder', 'file.txt');
         var pathToFolder = path.join(tmpPath, 'tstFolder');
         helper.guarantyFolder(pathToFile);
         t.ok(fs.existsSync(pathToFolder), 'Creates folder when given file path');
 
+        //Absolute path
+        var uri = './test';
+        var uriShould = path.resolve(process.cwd(), uri);
+        t.equal(helper.absolutePath(uri), uriShould, 'Resolves path to absolute relative to cwd');
+
+        //Loading of modules
+        var testPath = path.join(__dirname, 'assets', 'testModule.js');
+        var changedTestPath = path.join(__dirname, 'assets', 'testModuleChanged.js');
+        var tmpTestPath = path.join(__dirname, 'tmp', 'testModule.js');
+        fs.copySync(testPath, tmpTestPath);
+        var mod = helper.loadModule(tmpTestPath);
+        t.equal(mod.name, 'Tester', 'Loades the right module');
+        fs.copySync(changedTestPath, tmpTestPath);
+        var mod = helper.loadModule(tmpTestPath);
+        t.ok(mod.changed, 'Loades the changed module');
+
+
+        //Cleanup after ourselfes.
+        fs.removeSync(tmpPath);
+
         t.end();
 
 /*
         loadModule: loadModuleWithoutCache,
-        guarantyFolder: makeSureFolderExists,
-        absolutePath: getAbsolutePath
 */
     });
 
-    //Cleanup after ourselfes.
-    fs.removeSync(tmpPath);
+
 
 }
